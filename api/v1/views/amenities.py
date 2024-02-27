3c7ef63f7fd6531b55b62415264cb8ae1b536e5c
ProvLeon <providence.leonard@gmail.com>
2024-02-27 08:24:20 +0000

    completed the API Endpoints for Amenities module

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""A Flask route that returns json status response of amenities module"""
+
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models import storage, amenity
+
+Amenity = amenity.Amenity
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """Returns all amenities object in json format"""
+    amenities = [amenity.to_dict() for amenity in storage.all('Amenity').values()]
+    return jsonify(amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """Retrieves a specific amenity by its ID"""
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict()), 200
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """This Deletes a specific amenity object by its id"""
+    amenity = storage.get('Amenity', amenity_id)
+    #if the amenity is not found, raise a 404 error
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
+    """this create a new amenity object"""
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    amenity = Amenity(**data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    """Updates an amenity object with the given id"""
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)
+
+    try:
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(amenity, key, value)
+        storage.save()
+        return jsonify(amenity.to_dict()), 200
+    except Exception as e:
+        abort(400, f'Error parsing JSON: {str(e)}')
---
+++ tests/test_api/test_v1/test_views/test_amenities.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""Test for api/v1/views/amenities.py"""
+import unittest
+from flask import json
+from api.v1.app import app
+from models import storage, amenity
+
+Amenity = amenity.Amenity
+
+class TestAmenitiesAPI(unittest.TestCase):
+    """
+    A Unittest to test the ameinities API
+    """
+    def setUp(self):
+        # Initialize Flask app test Amenity API
+        self.app = app.test_client()
+        self.amenity_data = {'name':'Test Amenity'}
+        self.amenity = Amenity(**self.amenity_data)
+        storage.new(self.amenity)
+        storage.save()
+
+    def tearDown(self):
+        # Reset the database after each test
+        storage.delete(self.amenity)
+        storage.save()
+
+    def test_get_amenities(self):
+        """Test case for retrieving all amenities"""
+        response = self.app.get('/api/v1/amenities')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertTrue(isinstance(data, list))
+
+    def test_get_amenity(self):
+        # Test case for retrieving a specific amenity
+        amenity = Amenity(name="Test Amenity")
+        storage.new(amenity)
+        storage.save()
+        response = self.app.get(f"/api/v1/amenities/{amenity.id}")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data['id'], amenity.id)
+        # Test case for retrieving a non-existent amenity
+        response = self.app.get('/api/v1/amenities/1000')
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_amenity(self):
+        # Test case for deleting an existing amenity
+        amenity = Amenity(name="Test Amenity")
+        storage.new(amenity)
+        storage.save()
+        response = self.app.delete(f'/api/v1/amenities/{amenity.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(storage.get('Amenity', amenity.id), None)
+
+        # Test Case for deleting a non-existent amenity
+        response = self.app.delete('/api/v1/amenities/1000')
+        self.assertEqual(response.status_code, 404)
+
+    def test_create_amenity(self):
+        # Test case for creating a new amenity
+        data = {'name': 'New Amenity'}
+        response = self.app.post('/api/v1/amenities', json=data)
+        self.assertEqual(response.status_code, 201)
+        amenity_id = json.loads(response.get_data(as_text=True))['id']
+        self.assertTrue(storage.get('Amenity', amenity_id))
+        # Test case for creating an amenity with missing data
+        response = self.app.post('/api/v1/amenities', json={})
+        self.assertEqual(response.status_code, 400)
+
+        # Test case for creating an amenity with an invalid json
+        response = self.app.post('/api/v1/amenities', json='invalid json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_amenities(self):
+        # Test case for updating an existing amenity
+        amenity = Amenity(name="Test Amenity Original")
+        storage.new(amenity)
+        storage.save()
+        data = {'name': 'Updated Amenity'}
+        response = self.app.put(f'/api/v1/amenities/{amenity.id}', json=data)
+        self.assertEqual(response.status_code, 200)
+        updated_amenity = storage.get('Amenity', amenity.id)
+        self.assertEqual(updated_amenity.name, 'Updated Amenity')
+        # Test case for updating a non-existent amenity
+        response = self.app.put('/api/v1/amenities/1000', json=data)
+        self.assertEqual(response.status_code, 404)
+
+        # Test case for updating an amenity with an invalid json
+        response = self.app.put(f'/api/v1/amenities/{amenity.id}', json='invalid json')
+        self.assertEqual(response.status_code, 400)
