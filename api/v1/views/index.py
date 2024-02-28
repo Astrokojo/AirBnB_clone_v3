f66bbd1ea327f2aef42f993f80aad891a0bb514f
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-02-28 20:53:27 +0000

    fix(index.py): fix get_count function
    changed the data type to a dictionary list because jsonify wasn't reading it otherwise
    
    the endpoint (api/v1/stats) works now

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,14 +11,16 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('stats', methods=['GET'])
 def get_count():
     """Retrieves the number of each object by type"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
-    )
+    obj = {
+        'amenities': storage.count("Amenity"),
+        'cities': storage.count("City"),
+        'places': storage.count("Place"),
+        'reviews': storage.count("Review"),
+        'states': storage.count("State"),
+        'users': storage.count("User")
+        }
+    
+    return jsonify(obj)
